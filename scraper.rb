7979d8ea13f2fb4cb14c7af12472c14207486a8e
Tony Bowden <tony@tmtm.com>
2015-08-15 17:20:56 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,41 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.xpath('//p[contains(.,"Waakan Magacayadii")]//following-sibling::p').map(&:text).each do |li|
+    li.sub!(' 9UMP', '(UMP')
+    cap = li.match(/^(\d+)\.?\s*(.*?)\s*\((.*?)[.,\s]+(.*?)\)/) or break
+
+    data = { 
+      name: cap[2],
+      party: cap[3].upcase,
+      area: cap[4],
+      # term: term,
+      # source: url,
+    }
+    data[:area] = 'Obock' if data[:area] == 'Ubock'
+    ScraperWiki.save_sqlite([:name, :area], data)
+  end
+end
+
+scrape_list('http://www.lughaya.com/archives/19857')
